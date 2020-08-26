b937cd3e7fa9d165bc5c8f3bb6a7fbcffd99da67
Shafee <shafeelinks@gmail.com>
2020-08-26 04:35:26 -0400

    fixed fizzbuzz.js, starts at 1 now. doesn't display numbers for fizzbuzz

--- projects/Text/fizzbuzz.js
+++ projects/Text/fizzbuzz.js
@@ -2,16 +2,16 @@
 // But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 	if (i % 3 === 0 && i % 5 === 0) {
 		let fizzbuzz = "Fizzbuzz";
-		console.log(i, fizzbuzz);
+		console.log(fizzbuzz);
 	} else if (i % 3 === 0) {
 		let fizz = "Fizz";
-		console.log(i, fizz);
+		console.log(fizz);
 	} else if (i % 5 === 0) {
 		let buzz = "Buzz";
-		console.log(i, buzz);
+		console.log(buzz);
 	} else {
 		console.log(i);
 	}
