d98e350ae93c50922a09bcdb63e44d98195c22a1
Shafee <shafeelinks@gmail.com>
2020-08-26 04:30:31 -0400

    count words in a string, part 1 done

--- projects/Text/count-string.js
+++ projects/Text/count-string.js
@@ -1,3 +1,3 @@
 let str = prompt("Type something: ");
 let strArr = str.split(" ");
-console.log(`There were ${strArr.length} in your input.`);
+console.log(`There were ${strArr.length} words in your input.`);
